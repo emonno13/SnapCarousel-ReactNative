ceb461024e8645671bfa3204a50de3b0aa12a55d
emonno13 <emonno13@gmail.com>
2020-02-05 14:02:48 +0700

    first commit

---
+++ component/SnapCarousel.js
@@ -0,0 +1,584 @@
+import React, { Component } from 'react';
+import { Platform, View, ScrollView, Text, StatusBar, SafeAreaView,Dimensions,StyleSheet } from 'react-native';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+//import { sliderWidth, itemWidth } from './SliderEntry.style';
+import SliderEntry from './SliderEntry';
+//import styles, { colors } from './index.style';
+import { ENTRIES1, ENTRIES2 } from '../static/entries'; //import data from file entries.js
+//import { scrollInterpolators, animatedStyles } from './animations';
+
+
+
+
+
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
+function wp (percentage) {
+    const value = (percentage * viewportWidth) / 100;
+    return Math.round(value);
+}
+
+const slideHeight = viewportHeight * 0.36;
+const slideWidth = wp(75);
+const itemHorizontalMargin = wp(2);
+
+const sliderWidth = viewportWidth;
+const itemWidth = slideWidth + itemHorizontalMargin * 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Animation
+function scrollInterpolator1 (index, carouselProps) {
+    const range = [3, 2, 1, 0, -1];
+    const inputRange = getInputRangeFromIndexes(range, index, carouselProps);
+    const outputRange = range;
+
+    return { inputRange, outputRange };
+}
+function animatedStyles1 (index, animatedValue, carouselProps) {
+    const sizeRef = carouselProps.vertical ? carouselProps.itemHeight : carouselProps.itemWidth;
+    const translateProp = carouselProps.vertical ? 'translateY' : 'translateX';
+
+    return {
+        zIndex: carouselProps.data.length - index,
+        opacity: animatedValue.interpolate({
+            inputRange: [2, 3],
+            outputRange: [1, 0],
+            extrapolate: 'clamp'
+        }),
+        transform: [{
+            rotate: animatedValue.interpolate({
+                inputRange: [-1, 0, 1, 2, 3],
+                outputRange: ['-25deg', '0deg', '-3deg', '1.8deg', '0deg'],
+                extrapolate: 'clamp'
+            })
+        }, {
+            [translateProp]: animatedValue.interpolate({
+                inputRange: [-1, 0, 1, 2, 3],
+                outputRange: [
+                    -sizeRef * 0.5,
+                    0,
+                    -sizeRef, // centered
+                    -sizeRef * 2, // centered
+                    -sizeRef * 3 // centered
+                ],
+                extrapolate: 'clamp'
+            })
+        }]
+    };
+}
+
+// Perspective effect
+function scrollInterpolator2 (index, carouselProps) {
+    const range = [2, 1, 0, -1];
+    const inputRange = getInputRangeFromIndexes(range, index, carouselProps);
+    const outputRange = range;
+
+    return { inputRange, outputRange };
+}
+function animatedStyles2 (index, animatedValue, carouselProps) {
+    const sizeRef = carouselProps.vertical ? carouselProps.itemHeight : carouselProps.itemWidth;
+    const translateProp = carouselProps.vertical ? 'translateY' : 'translateX';
+
+    return {
+        zIndex: carouselProps.data.length - index,
+        opacity: animatedValue.interpolate({
+            inputRange: [-1, 0, 1, 2],
+            outputRange: [0.75, 1, 0.6, 0.4]
+        }),
+        transform: [{
+            rotate: animatedValue.interpolate({
+                inputRange: [-1, 0, 1, 2],
+                outputRange: ['0deg', '0deg', '5deg', '8deg'],
+                extrapolate: 'clamp'
+            })
+        }, {
+            scale: animatedValue.interpolate({
+                inputRange: [-1, 0, 1, 2],
+                outputRange: [0.96, 1, 0.85, 0.7]
+            })
+        }, {
+            [translateProp]: animatedValue.interpolate({
+                inputRange: [-1, 0, 1, 2],
+                outputRange: [
+                    0,
+                    0,
+                    -sizeRef + 30,
+                    -sizeRef * 2 + 45
+                ],
+                extrapolate: 'clamp'
+            })
+        }]
+    };
+}
+
+// Left/right translate effect
+function scrollInterpolator3 (index, carouselProps) {
+    const range = [2, 1, 0, -1];
+    const inputRange = getInputRangeFromIndexes(range, index, carouselProps);
+    const outputRange = range;
+
+    return { inputRange, outputRange };
+}
+function animatedStyles3 (index, animatedValue, carouselProps) {
+    const sizeRef = carouselProps.vertical ? carouselProps.itemHeight : carouselProps.itemWidth;
+    const translateProp = carouselProps.vertical ? 'translateY' : 'translateX';
+
+    return {
+        zIndex: carouselProps.data.length - index,
+        opacity: animatedValue.interpolate({
+            inputRange: [-1, 0, 1, 2],
+            outputRange: [1, 1, 0.75, 0.5],
+            extrapolate: 'clamp'
+        }),
+        transform: [{
+            [translateProp]: animatedValue.interpolate({
+                inputRange: [-1, 0, 1, 2],
+                outputRange: [
+                    0,
+                    0,
+                    -sizeRef * 2,
+                    -sizeRef
+                ],
+                extrapolate: 'clamp'
+            })
+        }]
+    };
+}
+
+// From https://codeburst.io/horizontal-scroll-animations-in-react-native-18dac6e9c720
+function scrollInterpolator4 (index, carouselProps) {
+    const range = [1, 0, -1];
+    const inputRange = getInputRangeFromIndexes(range, index, carouselProps);
+    const outputRange = range;
+
+    return { inputRange, outputRange };
+}
+function animatedStyles4 (index, animatedValue, carouselProps) {
+    return {
+        zIndex: carouselProps.data.length - index,
+        opacity: animatedValue.interpolate({
+            inputRange: [-1, 0, 1],
+            outputRange: [0.75, 1, 0.75],
+            extrapolate: 'clamp'
+        }),
+        transform: [
+            {
+                perspective: 1000
+            },
+            {
+                scale: animatedValue.interpolate({
+                    inputRange: [-1, 0, 1],
+                    outputRange: [0.65, 1, 0.65],
+                    extrapolate: 'clamp'
+                })
+            },
+            {
+                rotateX: animatedValue.interpolate({
+                    inputRange: [-1, 0, 1],
+                    outputRange: ['30deg', '0deg', '30deg'],
+                    extrapolate: 'clamp'
+                })
+            },
+            {
+                rotateY: animatedValue.interpolate({
+                    inputRange: [-1, 0, 1],
+                    outputRange: ['-30deg', '0deg', '30deg'],
+                    extrapolate: 'clamp'
+                })
+            }
+        ]
+    };
+}
+
+ const scrollInterpolators = {
+    scrollInterpolator1,
+    scrollInterpolator2,
+    scrollInterpolator3,
+    scrollInterpolator4
+};
+
+const animatedStyles = {
+    animatedStyles1,
+    animatedStyles2,
+    animatedStyles3,
+    animatedStyles4
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Colors & Styles
+const colors = {
+    black: '#1a1917',
+    gray: '#888888',
+    background1: '#B721FF',
+    background2: '#21D4FD'
+};
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: colors.black
+    },
+    container: {
+        flex: 1,
+        backgroundColor: colors.background1
+    },
+    gradient: {
+        ...StyleSheet.absoluteFillObject
+    },
+    scrollview: {
+        flex: 1
+    },
+    exampleContainer: {
+        paddingVertical: 30
+    },
+    exampleContainerDark: {
+        backgroundColor: colors.black
+    },
+    exampleContainerLight: {
+        backgroundColor: 'white'
+    },
+    title: {
+        paddingHorizontal: 30,
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.9)',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    titleDark: {
+        color: colors.black
+    },
+    subtitle: {
+        marginTop: 5,
+        paddingHorizontal: 30,
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.75)',
+        fontSize: 13,
+        fontStyle: 'italic',
+        textAlign: 'center'
+    },
+    slider: {
+        marginTop: 15,
+        overflow: 'visible' // for custom animations
+    },
+    sliderContentContainer: {
+        paddingVertical: 10 // for custom animation
+    },
+    paginationContainer: {
+        paddingVertical: 8
+    },
+    paginationDot: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        marginHorizontal: 8
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const IS_ANDROID = Platform.OS === 'android';
+const SLIDER_1_FIRST_ITEM = 1;
+
+export default class SnapCarousel extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            slider1ActiveSlide: SLIDER_1_FIRST_ITEM
+        };
+    }
+
+    _renderItem ({item, index}) {
+        return <SliderEntry data={item} even={(index + 1) % 2 === 0} />;
+    }
+
+    _renderItemWithParallax ({item, index}, parallaxProps) {
+        return (
+            <SliderEntry
+              data={item}
+              even={(index + 1) % 2 === 0}
+              parallax={true}
+              parallaxProps={parallaxProps}
+            />
+        );
+    }
+
+    _renderLightItem ({item, index}) {
+        return <SliderEntry data={item} even={false} />;
+    }
+
+    _renderDarkItem ({item, index}) {
+        return <SliderEntry data={item} even={true} />;
+    }
+
+    mainExample (number, title) {
+        const { slider1ActiveSlide } = this.state;
+
+        return (
+            <View style={styles.exampleContainer}>
+             {/* <Text style={styles.title}>{`Example ${number}`}</Text>
+                <Text style={styles.subtitle}>{title}</Text> */}
+                <Carousel
+                  ref={c => this._slider1Ref = c}
+                  data={ENTRIES1}
+                  renderItem={this._renderItemWithParallax}
+                  sliderWidth={sliderWidth}
+                  itemWidth={itemWidth}
+                  hasParallaxImages={true}
+                  firstItem={SLIDER_1_FIRST_ITEM}
+                  inactiveSlideScale={0.94}
+                  inactiveSlideOpacity={0.7}
+                  // inactiveSlideShift={20}
+                  containerCustomStyle={styles.slider}
+                  contentContainerCustomStyle={styles.sliderContentContainer}
+                  loop={true}
+                  loopClonesPerSide={2}
+                  autoplay={true}
+                  autoplayDelay={500}
+                  autoplayInterval={3000}
+                  onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index }) }
+                />
+                <Pagination
+                  dotsLength={ENTRIES1.length}
+                  activeDotIndex={slider1ActiveSlide}
+                  containerStyle={styles.paginationContainer}
+                  dotColor={'rgba(255, 255, 255, 0.92)'}
+                  dotStyle={styles.paginationDot}
+                  inactiveDotColor={colors.black}
+                  inactiveDotOpacity={0.4}
+                  inactiveDotScale={0.6}
+                  carouselRef={this._slider1Ref}
+                  tappableDots={!!this._slider1Ref}
+                />
+            </View>
+        );
+    }
+
+    momentumExample (number, title) {
+        return (
+            <View style={styles.exampleContainer}>
+                <Text style={styles.title}>{`Example ${number}`}</Text>
+                <Text style={styles.subtitle}>{title}</Text>
+                <Carousel
+                  data={ENTRIES2}
+                  renderItem={this._renderItem}
+                  sliderWidth={sliderWidth}
+                  itemWidth={itemWidth}
+                  inactiveSlideScale={0.95}
+                  inactiveSlideOpacity={1}
+                  enableMomentum={true}
+                  activeSlideAlignment={'start'}
+                  containerCustomStyle={styles.slider}
+                  contentContainerCustomStyle={styles.sliderContentContainer}
+                  activeAnimationType={'spring'}
+                  activeAnimationOptions={{
+                      friction: 4,
+                      tension: 40
+                  }}
+                />
+            </View>
+        );
+    }
+
+    layoutExample (number, title, type) {
+        const isTinder = type === 'tinder';
+        return (
+            <View style={[styles.exampleContainer, isTinder ? styles.exampleContainerDark : styles.exampleContainerLight]}>
+                <Text style={[styles.title, isTinder ? {} : styles.titleDark]}>{`Example ${number}`}</Text>
+                <Text style={[styles.subtitle, isTinder ? {} : styles.titleDark]}>{title}</Text>
+                <Carousel
+                  data={isTinder ? ENTRIES2 : ENTRIES1}
+                  renderItem={isTinder ? this._renderLightItem : this._renderItem}
+                  sliderWidth={sliderWidth}
+                  itemWidth={itemWidth}
+                  containerCustomStyle={styles.slider}
+                  contentContainerCustomStyle={styles.sliderContentContainer}
+                  layout={type}
+                  loop={true}
+                />
+            </View>
+        );
+    }
+
+    customExample (number, title, refNumber, renderItemFunc) {
+        const isEven = refNumber % 2 === 0;
+
+        // Do not render examples on Android; because of the zIndex bug, they won't work as is
+        return !IS_ANDROID ? (
+            <View style={[styles.exampleContainer, isEven ? styles.exampleContainerDark : styles.exampleContainerLight]}>
+                <Text style={[styles.title, isEven ? {} : styles.titleDark]}>{`Example ${number}`}</Text>
+                <Text style={[styles.subtitle, isEven ? {} : styles.titleDark]}>{title}</Text>
+                <Carousel
+                  data={isEven ? ENTRIES2 : ENTRIES1}
+                  renderItem={renderItemFunc}
+                  sliderWidth={sliderWidth}
+                  itemWidth={itemWidth}
+                  containerCustomStyle={styles.slider}
+                  contentContainerCustomStyle={styles.sliderContentContainer}
+                  scrollInterpolator={scrollInterpolators[`scrollInterpolator${refNumber}`]}
+                  slideInterpolatedStyle={animatedStyles[`animatedStyles${refNumber}`]}
+                  useScrollView={true}
+                />
+            </View>
+        ) : false;
+    }
+
+    // get gradient () {
+    //     return (
+    //         <LinearGradient
+    //           colors={[colors.background1, colors.background2]}
+    //           startPoint={{ x: 1, y: 0 }}
+    //           endPoint={{ x: 0, y: 1 }}
+    //           style={styles.gradient}
+    //         />
+    //     );
+    // }
+
+    render () {
+        const example1 = this.mainExample(1, 'Default layout | Loop | Autoplay | Parallax | Scale | Opacity | Pagination with tappable dots');
+        const example2 = this.momentumExample(2, 'Momentum | Left-aligned | Active animation');
+        const example3 = this.layoutExample(3, '"Stack of cards" layout | Loop', 'stack');
+        const example4 = this.layoutExample(4, '"Tinder-like" layout | Loop', 'tinder');
+        const example5 = this.customExample(5, 'Custom animation 1', 1, this._renderItem);
+        const example6 = this.customExample(6, 'Custom animation 2', 2, this._renderLightItem);
+        const example7 = this.customExample(7, 'Custom animation 3', 3, this._renderDarkItem);
+        const example8 = this.customExample(8, 'Custom animation 4', 4, this._renderLightItem);
+
+        return (
+            <SafeAreaView style={styles.safeArea}>
+                <View style={styles.container}>
+                    <StatusBar
+                      translucent={true}
+                      backgroundColor={'rgba(0, 0, 0, 0.3)'}
+                      barStyle={'light-content'}
+                    />
+                    {/* { this.gradient } */}
+                    <ScrollView
+                      style={styles.scrollview}
+                      scrollEventThrottle={200}
+                      directionalLockEnabled={true}
+                    >
+                        { this.mainExample() }
+                        { example2 }
+                        { example3 }
+                        { example4 }
+                        { example5 }
+                        { example6 }
+                        { example7 }
+                        { example8 }
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
